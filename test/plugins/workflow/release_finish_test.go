e3315eddc9935ea9b622ed31603520998bc87a4f
Gleb Levitin <gleb.levitin@mercedes-benz.com>
2025-05-27 14:15:43 +0200

    Refactor build workflow and tests for version template handling
    Gleb Levitin <gleb.levitin@mercedes-benz.com>, Mercedes-Benz Tech Innovation GmbH

--- test/plugins/workflow/release_finish_test.go
+++ test/plugins/workflow/release_finish_test.go
@@ -60,35 +60,35 @@ func testReleaseFinish(t *testing.T, templateName string, versionQualifier strin
 }
 
 // TestReleaseFinishWithoutVersionFileInMain (test fallback to standard plugin with additional functionality)
-//func TestReleaseFinishWithoutVersionFileInMain(t *testing.T) {
-//	// GIVEN: a Git repository with production and development branch
-//	env := helper.SetupTestEnv(t)
-//
-//	// Path to the version file template
-//	template := filepath.Join("../..", "helper", "templates", "version.txt.tpl")
-//
-//	// main -> no version file
-//	// develop -> version.txt (1.0.0-dev)
-//	// release/1.0.0 -> version.txt (1.0.0)
-//
-//	env.CommitFileFromTemplate(template, "1.0.0-dev", "develop")
-//	env.CreateBranch("release/1.0.0", "develop")
-//	env.CommitFileFromTemplate(template, "1.0.0", "release/1.0.0")
-//
-//	// WHEN
-//	env.ExecuteGitflow("release", "finish")
-//
-//	// THEN
-//	// Check main branch state
-//	env.AssertCommitMessageEquals("Merge branch 'release/1.0.0'", "main")
-//	env.AssertTagEquals("1.0.0", "main")
-//	env.AssertVersionEquals(template, "1.0.0", "main")
-//
-//	// Check develop branch state
-//	env.AssertCommitMessageEquals("Merge branch 'release/1.0.0' into develop", "develop", 1)
-//	env.AssertCommitMessageEquals("Set next minor project version.", "develop", 0)
-//	env.AssertVersionEquals(template, "1.1.0-dev", "develop")
-//
-//	env.AssertBranchDoesNotExist("release/1.0.0")
-//	env.AssertCurrentBranchEquals("develop")
-//}
+func TestReleaseFinishWithoutVersionFileInMain(t *testing.T) {
+	// GIVEN: a Git repository with production and development branch
+	env := helper.SetupTestEnv(t)
+
+	// Path to the version file template
+	template := filepath.Join("../..", "helper", "templates", "version.txt.tpl")
+
+	// main -> no version file
+	// develop -> version.txt (1.0.0-dev)
+	// release/1.0.0 -> version.txt (1.0.0)
+
+	env.CommitFileFromTemplate(template, "1.0.0-dev", "develop")
+	env.CreateBranch("release/1.0.0", "develop")
+	env.CommitFileFromTemplate(template, "1.0.0", "release/1.0.0")
+
+	// WHEN
+	env.ExecuteGitflow("release", "finish")
+
+	// THEN
+	// Check main branch state
+	env.AssertCommitMessageEquals("Merge branch 'release/1.0.0'", "main")
+	env.AssertTagEquals("1.0.0", "main")
+	env.AssertVersionEquals(template, "1.0.0", "main")
+
+	// Check develop branch state
+	env.AssertCommitMessageEquals("Merge branch 'release/1.0.0' into develop", "develop", 1)
+	env.AssertCommitMessageEquals("Set next minor project version.", "develop", 0)
+	env.AssertVersionEquals(template, "1.1.0-dev", "develop")
+
+	env.AssertBranchDoesNotExist("release/1.0.0")
+	env.AssertCurrentBranchEquals("develop")
+}
